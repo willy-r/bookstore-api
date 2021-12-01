121ad1516f97b646bd9a807d888473a07c652404
William Rodrigues <williamrodrigues2442@gmail.com>
2021-12-01 14:27:29 -0300

    Changes mock data informations

--- src/infra/schemas.js
+++ src/infra/schemas.js
@@ -17,15 +17,15 @@ const populateBookTable = `
   INSERT INTO livro
     (ISBN, titulo, descricao, url_img, preco, paginas, ano_publicacao, id_editora, id_autor)
   VALUES
-    ('1234567891', 'Mussum Ipsum', 'Mussum Ipsum', 'https://m.media-amazon.com/images/I/51qnfeR7uCL.jpg', 69.69, 666, 2013, 1, 1),
-    ('1234567892', 'Mussum Ipsum', 'Mussum Ipsum', 'https://m.media-amazon.com/images/I/51qnfeR7uCL.jpg', 69.69, 666, 2013, 1, 1),
-    ('1234567893', 'Mussum Ipsum', 'Mussum Ipsum', 'https://m.media-amazon.com/images/I/51qnfeR7uCL.jpg', 69.69, 666, 2013, 1, 1),
-    ('1234567894', 'Mussum Ipsum', 'Mussum Ipsum', 'https://m.media-amazon.com/images/I/51qnfeR7uCL.jpg', 69.69, 666, 2013, 2, 2),
-    ('1234567895', 'Mussum Ipsum', 'Mussum Ipsum', 'https://m.media-amazon.com/images/I/51qnfeR7uCL.jpg', 69.69, 666, 2013, 2, 2),
-    ('1234567896', 'Mussum Ipsum', 'Mussum Ipsum', 'https://m.media-amazon.com/images/I/51qnfeR7uCL.jpg', 69.69, 666, 2013, 2, 2),
-    ('1234567897', 'Mussum Ipsum', 'Mussum Ipsum', 'https://m.media-amazon.com/images/I/51qnfeR7uCL.jpg', 69.69, 666, 2013, 3, 3),
-    ('1234567898', 'Mussum Ipsum', 'Mussum Ipsum', 'https://m.media-amazon.com/images/I/51qnfeR7uCL.jpg', 69.69, 666, 2013, 3, 3),
-    ('1234567899', 'Mussum Ipsum', 'Mussum Ipsum', 'https://m.media-amazon.com/images/I/51qnfeR7uCL.jpg', 69.69, 666, 2013, 3, 3)
+    ('1234567891', 'Mussum Ipsum', 'In elementis mé pra quem é amistosis quis leo.', 'https://m.media-amazon.com/images/I/51qnfeR7uCL.jpg', 68.68, 555, 2013, 1, 1),
+    ('1234567892', 'Mussum Ipsum', 'In elementis mé pra quem é amistosis quis leo.', 'https://m.media-amazon.com/images/I/51qnfeR7uCL.jpg', 68.68, 555, 2013, 1, 1),
+    ('1234567893', 'Mussum Ipsum', 'In elementis mé pra quem é amistosis quis leo.', 'https://m.media-amazon.com/images/I/51qnfeR7uCL.jpg', 68.68, 555, 2013, 1, 1),
+    ('1234567894', 'Mussum Ipsum', 'In elementis mé pra quem é amistosis quis leo.', 'https://m.media-amazon.com/images/I/51qnfeR7uCL.jpg', 68.68, 555, 2013, 2, 2),
+    ('1234567895', 'Mussum Ipsum', 'In elementis mé pra quem é amistosis quis leo.', 'https://m.media-amazon.com/images/I/51qnfeR7uCL.jpg', 68.68, 555, 2013, 2, 2),
+    ('1234567896', 'Mussum Ipsum', 'In elementis mé pra quem é amistosis quis leo.', 'https://m.media-amazon.com/images/I/51qnfeR7uCL.jpg', 68.68, 555, 2013, 2, 2),
+    ('1234567897', 'Mussum Ipsum', 'In elementis mé pra quem é amistosis quis leo.', 'https://m.media-amazon.com/images/I/51qnfeR7uCL.jpg', 68.68, 555, 2013, 3, 3),
+    ('1234567898', 'Mussum Ipsum', 'In elementis mé pra quem é amistosis quis leo.', 'https://m.media-amazon.com/images/I/51qnfeR7uCL.jpg', 68.68, 555, 2013, 3, 3),
+    ('1234567899', 'Mussum Ipsum', 'In elementis mé pra quem é amistosis quis leo.', 'https://m.media-amazon.com/images/I/51qnfeR7uCL.jpg', 68.68, 555, 2013, 3, 3)
   ;
 `;
 
